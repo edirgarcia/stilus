7328c0fca33a1855c037ce8c1772f880c7e44151
Edir Garcia <edirgl@hotmail.com>
2020-01-26 12:07:51 -0800

    Add Mozart and Bach

--- scripts/get_training_data.py
+++ scripts/get_training_data.py
@@ -6,21 +6,21 @@ import numpy as np
 path = "../midi/training/"
 result = None
 
-for file in os.listdir(path):
+for root, dirs, files in os.walk(path):
 
-    #import a midi file
-    abs_path = os.path.join(path, file)
-    print(abs_path)
-    mid = MidiFile(abs_path)
+    for file in files :
+        abs_path = os.path.join(root, file)
+        print(abs_path)
+        mid = MidiFile(abs_path)
 
-    time_series = convert_midi_to_time_series(mid, 5, 5, 8)
-    training_data = get_training_data(time_series, 33)
+        time_series = convert_midi_to_time_series(mid, 5, 5, 8)
+        training_data = get_training_data(time_series, 33)
 
-    #print(training_data)
-    if result is None :
-        result = training_data
-    else :
-        result = np.vstack((result, training_data))
+        #print(training_data)
+        if result is None :
+            result = training_data
+        else :
+            result = np.vstack((result, training_data))
 
 print(result.shape)
 
