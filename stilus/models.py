f75d0d627d33f5c42843a13b891eb6db095e52c7
Edir Garcia <edirgl@hotmail.com>
2020-03-29 14:20:42 -0700

    Analysis on weighted MSELoss

--- stilus/models.py
+++ stilus/models.py
@@ -20,15 +20,19 @@ class WeightedL1Loss(nn.Module):
     def __init__(self):
         super(WeightedL1Loss, self).__init__()
         self.internal_loss = nn.L1Loss()
-        self.importance = torch.tensor([[1.0], [1.0], [1.0], [1.0], [1.0]], requires_grad=True)
+
+        cuda0 = torch.device('cuda:0')
+
+        self.importance = torch.tensor([[1.0], [1.0], [1.0], [1.0], [1.0]], requires_grad=True, device=cuda0 )
         
     def forward(self, y_hat, y):
         
-        print(y_hat.shape)
-        print(self.importance.shape)
-        x = torch.mul(y_hat, self.importance)
+        #print("y_hat->",y_hat.shape)
+        #print("importance->",self.importance.shape)
+        x = y_hat * self.importance[:, None].T
+        #print("x->",x.shape)
         
-        return self.internal_loss(x, y)
+        return self.internal_loss(x[0], y)
     
 
 class AbstractMidiNet(pl.LightningModule):
